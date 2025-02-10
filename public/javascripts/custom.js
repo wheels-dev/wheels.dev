f7c0fc1a15274f3db6a530cac1839f683faaa9ef
Zain Ul Abideen <115699497+zainforbjs@users.noreply.github.com>
2025-02-10 19:45:24 +0500

    implemented home page html design

---
+++ public/javascripts/custom.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".blogSwiper", {
+    spaceBetween: 30,
+    freeMode: true,
+    initialSlide: 2,
+    slidesOffsetBefore: 250,
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+});
